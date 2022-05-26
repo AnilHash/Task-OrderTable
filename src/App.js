6e45e85bf923f844fc18cd834de2b8a2c3aa7c18
AnilHash <anilaraganji123@gmail.com>
2022-05-27 03:51:52 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { createContext, useState } from "react";
+import CsvReader from "./components/CsvReader";
+import MainComponent from "./components/MainComponent";
+import { OrderContext } from "./utilities/helper";
+
+const App = () => {
+  const [orderList, setOrderList] = useState([]);
+  return (
+    <div className="App">
+      <h2>Order Table</h2>
+      <OrderContext.Provider value={{ orderList, setOrderList }}>
+        <CsvReader />
+        <MainComponent />
+      </OrderContext.Provider>
+    </div>
+  );
+};
+
+export default App;
